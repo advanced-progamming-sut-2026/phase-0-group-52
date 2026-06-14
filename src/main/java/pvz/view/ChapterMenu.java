59237766aa9667108ea718b578e3fef6f51b1827
imanniknam <imanvid2334@gmail.com>
2026-06-14 23:43:21 +0330

    feature: newsmenu + settingmenu + chaptermenu + mainmenu completed

--- src/main/java/pvz/view/ChapterMenu.java
+++ src/main/java/pvz/view/ChapterMenu.java
@@ -1,4 +1,24 @@
 package pvz.view;
 
-public class ChapterMenu implements AppMenu{
+public class ChapterMenu implements AppMenu {
+
+    public void showEnteredChapter(String chapterName) {
+        System.out.println("Entered chapter: " + chapterName);
+    }
+
+    public void showCoinWallet(int coins) {
+        System.out.println("Coin Wallet: " + coins + " coins");
+    }
+
+    public void showGemWallet(int gems) {
+        System.out.println("Gem Wallet: " + gems + " gems");
+    }
+
+    public void showCheatResult(int amount, String type, int newBalance) {
+        System.out.println("Added " + amount + " " + type + "(s). New balance: " + newBalance);
+    }
+
+    public void showError(String message) {
+        System.out.println("Error: " + message);
+    }
 }
