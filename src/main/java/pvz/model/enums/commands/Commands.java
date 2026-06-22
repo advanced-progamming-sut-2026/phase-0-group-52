776e9b02106848579e9673bb4ed90a9db5e0c8c9
Amirali Keshavarz <amirraliik24@gmail.com>
2026-06-22 17:56:24 +0330

    Add SignUp and Login controllers؛

---
+++ src/main/java/pvz/model/enums/commands/Commands.java
@@ -0,0 +1,23 @@
+package pvz.model.enums.commands;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface Commands {
+
+
+    String getPattern();
+    default Matcher getMatcher(String input) {
+        Matcher matcher = Pattern.compile(getPattern()).matcher(input);
+        if (matcher.matches()) {
+            return matcher;
+        }
+        return null;
+    }
+    default boolean matches(String input) {
+        return Pattern.compile(getPattern()).matcher(input).matches();
+    }
+    default String getGroup(String input, String group) {
+        return getMatcher(input).group(group);
+    }
+}
